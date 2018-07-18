4f82e01c7a29929db7c1f0693240198e2c3a287a
PrettyKitty92 <patrycja.szelc@gmail.com>
2018-07-18 04:37:41 +0200

    fix redirect bug

--- src/app/register/register.service.ts
+++ src/app/register/register.service.ts
@@ -2,23 +2,27 @@
 import {Http, Headers, RequestOptions, Response} from '@angular/http';
 
 import {User} from '../_models/index';
+import {environment} from "../../environments/environment";
 
 @Injectable()
 export class RegisterService {
 
 
+
+  baseUrl = environment.baseUrl;
+
   constructor(private http: Http) {
   }
 
   singUpUser(user: User) {
-    return this.http.post(`/sign-up/user`, user, new RequestOptions({ headers: new Headers({'content-type': 'application/json'})}))
+    return this.http.post(this.baseUrl +`/sign-up/user`, user, new RequestOptions({ headers: new Headers({'content-type': 'application/json'})}))
       .toPromise()
       .then(response => response)
       .catch(this.handleError);
   }
 
   singUpAdmin(user: User) {
-    return this.http.post(`/sign-up/admin`, user, new RequestOptions({ headers: new Headers({'content-type': 'application/json'})}))
+    return this.http.post(this.baseUrl +`/sign-up/admin`, user, new RequestOptions({ headers: new Headers({'content-type': 'application/json'})}))
       .toPromise()
       .then(response => response)
       .catch(this.handleError);
